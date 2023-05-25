f31e3db7c5f7cafc5129bd90c99b39799c2fc8ca
Ivan Klimov <klimov128@gmail.com>
2023-05-25 15:48:51 +0300

    Create Movies.jsx

---
+++ src/pages/Movies.jsx
@@ -0,0 +1,12 @@
+export const Movies = () => {
+  return (
+    <div>
+      <form action="">
+        <label>
+          <input type="text" />
+          <button type="submit">Search</button>
+        </label>
+      </form>
+    </div>
+  );
+};
