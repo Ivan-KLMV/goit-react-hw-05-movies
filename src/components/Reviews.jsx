51b528643f390139bc444b01dbd6ecc553eed1c7
Ivan Klimov <klimov128@gmail.com>
2023-05-27 11:34:18 +0300

    Update Reviews.jsx
    add message 'We don't have any reviews for this movies'

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -5,6 +5,8 @@ export const Reviews = () => {
   // 634649
   const [reviews, setReviews] = useState([]);
   const { id } = useParams();
+  // const [isLoading, setIsLoading] = useState(false);
+  const [totalResults, setTotalResults] = useState(1);
 
   useEffect(() => {
     const options = {
@@ -15,6 +17,8 @@ export const Reviews = () => {
           'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMTI1YzAwN2Q3NzBjMmI2YTk2YTY5NGEyYWIxOWRjZSIsInN1YiI6IjY0NmNkNTE4MmJjZjY3MDE1NTg0Y2I0MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1zl0yET6waSnMi-_6LNcanZOg8qB1GH_Q87VGSPFtqc',
       },
     };
+
+    // setIsLoading(true);
 
     fetch(
       `https://api.themoviedb.org/3/movie/${id}/reviews?language=en-US&page=1`,
@@ -30,21 +34,27 @@ export const Reviews = () => {
       .then(response => {
         // console.log(response);
         setReviews(response.results);
+        setTotalResults(response.total_results);
+        // setIsLoading(false);
       })
       .catch(err => console.error('this is error', err));
   }, [id]);
-
   //   console.log(reviews);
+  //We don't have any reviewsfor this movies
   return (
     <div>
-      <ul>
-        {reviews.map(review => (
-          <li key={review.id}>
-            <h4>author: {review.author}</h4>
-            <p>{review.content}</p>
-          </li>
-        ))}
-      </ul>
+      {totalResults > 0 ? (
+        <ul>
+          {reviews.map(review => (
+            <li key={review.id}>
+              <h4>author: {review.author}</h4>
+              <p>{review.content}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>We don't have any reviews for this movies</p>
+      )}
     </div>
   );
 };
