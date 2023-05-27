42d2aecae0d3bab037bf38565b3d7623aae10c79
Ivan Klimov <klimov128@gmail.com>
2023-05-27 22:00:32 +0300

    Update MoviesList.jsx
    add useLocation

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -1,12 +1,17 @@
 import { Link } from 'react-router-dom';
 
-export const MoviesList = ({ movies }) => {
+export const MoviesList = ({ movies, path }) => {
+  // console.log('reserv', reserv);
+  // const backwardPath = path.state?.from ?? reserv;
+  // console.log('backwardPath', backwardPath);
   return (
     <div>
       <ul>
         {movies.map(movie => (
           <li key={movie.id}>
-            <Link to={`/movies/${movie.id}`}>{movie.original_title}</Link>
+            <Link to={`/movies/${movie.id}`} state={{ from: path }}>
+              {movie.original_title}
+            </Link>
           </li>
         ))}
       </ul>
