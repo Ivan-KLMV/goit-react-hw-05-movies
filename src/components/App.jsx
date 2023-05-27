30016e2d42b9a43f863bc108c00ead3ee8ea9149
Ivan Klimov <klimov128@gmail.com>
2023-05-28 00:58:57 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import { Movies } from '../pages/Movies';
-import { MovieDetails } from '../pages/MovieDetails';
-import { Cast } from './Cast';
+import { MovieDetails } from '../pages/MovieDetails/MovieDetails';
+import { Cast } from './Cast/Cast';
 import { Reviews } from './Reviews';
-import { Layout } from './Layout';
+import { Layout } from './Layout/Layout';
 import { NotFound } from './NotFound';
 
 export const App = () => {
